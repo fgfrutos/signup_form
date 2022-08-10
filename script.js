e7b4ae262614037fc49d518f5a854b3d9d1ab505
fgfrutos <fgfrutos@gmail.com>
2022-08-10 11:26:04 +0200

    Improved layout and validation

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const password = document.querySelectorAll('input[type="password"]');
 const errorText = document.querySelector('.label-input > p');
+const blankSpace = document.querySelector('.space');
 const submitBtn = document.querySelector('button');
 
-const checkEqual = (e) => {
-    if (password[0].value !== password[1].value) {
+const checkPassword = (e) => {
+    if (password[0].value.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/) === null) {
+        errorText.textContent = '* Invalid password'
+        password[0].classList.add('invalid');
+        e.preventDefault();
+    } else if (password[0].value !== password[1].value) {
         errorText.textContent = '* Password do not mach'
         password.forEach(input => input.classList.add('invalid'));
         e.preventDefault();
     }
+
+    blankSpace.style.display = 'block';
 }
 
-submitBtn.addEventListener('click', checkEqual);
+submitBtn.addEventListener('click', checkPassword);
